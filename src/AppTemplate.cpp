d0de03ede611714ab172831effa8a276cdd9fab4
severalgh <pdrzycimski@gmail.com>
2017-07-26 21:38:13 +0200

    Remove unnecessary code

--- src/AppTemplate.cpp
+++ src/AppTemplate.cpp
@@ -1,7 +1,5 @@
 #include "application/Application.h"
 
-#include <boost/program_options/options_description.hpp>
-
 class AppTemplate : public Application
 {
 public:
@@ -19,25 +17,12 @@ AppTemplate::AppTemplate(int argc, char *argv[])
     Application::showHelpIfNoArguments(); 
     Application::addCmdOption("option,o", "example of cmd option");
     Application::addCmdOptionFlag("flag,f", "example of cmd flag");
-    Application::addCmdOptionFlag("flag,f", "example of cmd flag");
 }
 
 using po = boost::program_options::option_description;
 int AppTemplate::main()
 {
-    std::cout << "AppTemplate\n";
-
-    po::option_description o("option,o", boost::program_options::value<std::string>());
-     
-    const auto res = o.match("option,o", true, true, true);
-    if(res == po::match_result::no_match)
-    {
-        std::cout << "no match\n";
-    }
-    else
-    {
-        std::cout << "match\n";
-    } 
+    std::cout << "AppTemplate\n"; 
     return 0;
 }
 
